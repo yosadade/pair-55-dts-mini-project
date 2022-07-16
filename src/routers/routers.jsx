f5d60567f414319a8e5f5f7e2c6115c3a6edfb89
yosadade <masyosad@gmail.com>
2022-07-16 10:14:34 +0700

    boilerplate

---
+++ src/routers/routers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={} /> */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routers;
